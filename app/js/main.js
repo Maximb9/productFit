72019e69ce12c27c01535c86b07cfc09539f392b
Maxim <bmw_077@mail.ru>
2021-09-20 19:19:46 +0300

    section orders

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,7 @@ $(function() {
         speed: 1000,
         slidesToShow: 1,
         arrows: true,
-        prevArrow: '<button type="button" class="banner__slick-arrow banner__slick-arrowleft"><img src="images/ab.svg" alt="arrowb"></button>',
-        nextArrow: '<button type="button" class="banner__slick-arrow banner__slick-arrowright"><img src="images/an.svg" alt="arrown"></button>'
-      });
+        nextArrow: '<button type="button" class="banner__slick-arrow banner__slick-arrowright"><img src="images/an.svg" alt="arrown"></button>',
+        prevArrow: '<button type="button" class="banner__slick-arrow banner__slick-arrowleft"><img src="images/ab.svg" alt="arrowb"></button>'
+    });
 })
