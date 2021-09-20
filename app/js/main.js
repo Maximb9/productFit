17e102969e1b22404928b62fbfc377db6519cd35
Maxim <bmw_077@mail.ru>
2021-09-20 16:58:25 +0300

    arrow

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.banner__slick').slick({
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        arrows: true,
+        prevArrow: '<button type="button" class="banner__slick-arrow banner__slick-arrowleft"><img src="images/ab.svg" alt="arrowb"></button>',
+        nextArrow: '<button type="button" class="banner__slick-arrow banner__slick-arrowright"><img src="images/an.svg" alt="arrown"></button>'
+      });
+})
