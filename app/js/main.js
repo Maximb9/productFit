2b50ec2fbac6754aec5a901085ac801a3cfd3add
Maxim <bmw_077@mail.ru>
2021-09-28 21:51:57 +0300

    error

--- app/js/main.js
+++ app/js/main.js
@@ -10,13 +10,13 @@ $(function() {
 
 
 
-    $('.slider__computer').slick({
-        infinite: true,
-        speed: 1000,
-        dots: true,
-        slidesToShow: 1,
-        arrows: true,
-        nextArrow: '<button type="button" class="slider__computer-arrow slider__computer-arrowright"><img src="images/an.svg" alt="arrown"></button>',
-        prevArrow: '<button type="button" class="slider__computer-arrow slider__computer-arrowleft"><img src="images/ab.svg" alt="arrowb"></button>'
+    $(function () {
+        $(".banner-section__slider").slick({
+          dots: true,
+          prevArrow:
+            '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
+          nextArrow:
+            '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>',
+        });
     });
-})
+});
