4d22f3c268b9874674275df25bdd947450ffa15b
Maxim <bmw_077@mail.ru>
2021-09-28 20:28:07 +0300

    slider page case

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,16 @@ $(function() {
         nextArrow: '<button type="button" class="banner__slick-arrow banner__slick-arrowright"><img src="images/an.svg" alt="arrown"></button>',
         prevArrow: '<button type="button" class="banner__slick-arrow banner__slick-arrowleft"><img src="images/ab.svg" alt="arrowb"></button>'
     });
+
+
+
+    $('.slider__computer').slick({
+        infinite: true,
+        speed: 1000,
+        dots: true,
+        slidesToShow: 1,
+        arrows: true,
+        nextArrow: '<button type="button" class="slider__computer-arrow slider__computer-arrowright"><img src="images/an.svg" alt="arrown"></button>',
+        prevArrow: '<button type="button" class="slider__computer-arrow slider__computer-arrowleft"><img src="images/ab.svg" alt="arrowb"></button>'
+    });
 })
