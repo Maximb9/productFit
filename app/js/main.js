d0f6145e55cb5566c771db4e9609263cd4dd89dc
Maxim <bmw_077@mail.ru>
2021-10-25 23:45:41 +0300

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -19,4 +19,14 @@ $(function() {
             '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/next.svg" alt=""></button>',
         });
     });
+
+    $(function () {
+      $(".banner-section__slider").slick({
+        dots: true,
+        prevArrow:
+          '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/prev.svg" alt=""></button>',
+        nextArrow:
+          '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/next.svg" alt=""></button>',
+      });
+  });
 });
